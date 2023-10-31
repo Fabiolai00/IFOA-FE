b992a3f5687f7936653a4ce5cf75cc55752da566
Fabiolai00 <148853890+Fabiolai00@users.noreply.github.com>
2023-10-31 17:05:03 +0100

    esercizio2 aggiornato

--- Settimana_2/Giorno_2/Esercizio_2/D2.js
+++ Settimana_2/Giorno_2/Esercizio_2/D2.js
@@ -211,12 +211,25 @@ console.log(me);
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+delete me.skills[2];
+console.log(me.skills);
+
 
 /* ESERCIZIO 14
   Scrivi del codice per creare un array inizialmente vuoto. Riempilo successivamente con i numeri da 1 a 10.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+const array = [];
+console.log(array);
+
+array[0] = 1;
+array[1] = 2;
+array[2] = 3;
+array[3] = 4;
+array[4] = 5;
+console.log(array);
 
 /* ESERCIZIO 15
   Scrivi del codice per sostituire l'ultimo elemento dell'array, ovvero il valore 10, con il valore 100.
